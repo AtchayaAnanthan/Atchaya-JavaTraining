79f2e78df39e9031cb6f11b197c3ba64bf705c37
AtchayaAnanthan <102713564+AtchayaAnanthan@users.noreply.github.com>
2022-03-31 12:01:11 +0530

    Ribbon-app-1-implementation

---
+++ ribbon-app-1-implementation/src/test/java/mypkg/RibbonApp1ImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package mypkg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibbonApp1ImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
