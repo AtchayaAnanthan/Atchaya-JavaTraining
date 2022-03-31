153eada5fa468c4a47f99b78b679dad991f284ab
AtchayaAnanthan <102713564+AtchayaAnanthan@users.noreply.github.com>
2022-03-31 11:39:13 +0530

    ECommerceWithDependency1

---
+++ ECommerceWithDependency1/src/main/java/mypkg/ECommerceWithDependency1Application.java
@@ -0,0 +1,13 @@
+package mypkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceWithDependency1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceWithDependency1Application.class, args);
+	}
+
+}
